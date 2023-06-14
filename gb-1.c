95ed692e562dce365caaef6ed003810409b1dffd
Stephen Justin <stevejustin1963@gmail.com>
2023-06-14 14:38:12 +1000

    Update gb-1.c

--- gb-1.c
+++ gb-1.c
@@ -45,3 +45,75 @@ int main() {
 
     return 0;
 }
+///////////////////////////////////////////
+
+Looking at the provided C code, your Forth program mostly translates the functionality of the C code correctly, but some adjustments are necessary to handle memory allocation and initialisation:
+
+1. The bins are not initialised to zero in the Forth code. This can be accomplished with a loop.
+
+2. The random number function in Forth may work differently from C's `rand() % 2`. You need to make sure that the `rand` word generates a uniformly distributed random number either 0 or 1.
+
+Here's a version of the Forth code which incorporates these changes:
+
+```forth
+10 CONSTANT PEGS
+11 CONSTANT BINS
+
+CREATE bins BINS CELLS ALLOT
+
+100 CONSTANT numBalls
+
+: srand  ( -- )  GET-TIME  SEED RANDOM ;
+: rand  ( -- n )  RANDOM  2 MOD ;
+
+: init-bins ( -- )
+    BINS 0 DO
+        0 I bins CELLS + !
+    LOOP ;
+
+: adjust-position  ( n -- n' )  DUP  0< IF  DROP 0 ELSE  DUP BINS >= IF  DROP BINS 1- ELSE  THEN THEN ;
+
+: simulate-balls  ( -- )
+    numBalls 0 DO
+        0 ( starting position )
+        PEGS 0 DO
+            rand IF 1+ ELSE 1- THEN ( move left or right )
+        LOOP
+        adjust-position
+        bins SWAP CELLS + @ 1+ SWAP bins SWAP CELLS + ! ( increment the corresponding bin )
+    LOOP ;
+
+: print-bins  ( -- )
+    ." Bins:" CR
+    BINS 0 DO
+        I 2 .R ." : " 
+        bins I CELLS + @ .
+        CR
+    LOOP ;
+
+: main  ( -- )
+    srand
+    init-bins
+    simulate-balls
+    print-bins ;
+
+main
+```
+
+The `init-bins` word initialises the bins to 0. In `simulate-balls`, the bin corresponding 
+to the final position of the ball is incremented by first fetching the current count
+(`bins SWAP CELLS + @`), incrementing it (`1+`), and then storing it back in the same 
+bin (`SWAP bins SWAP CELLS + !`). This sequence is necessary because Forth doesn't have a `+!` 
+operation that works directly on cell-based arrays. 
+The `print-bins` word has been adjusted to match the output format of the C program.
+
+This version of the Forth program should behave more similarly to the C code. 
+Again, it assumes that your Forth system has the words GET-TIME, SEED RANDOM, RANDOM, 
+CREATE, ALLOT, CELLS, +!, @, ., .R, CR, and that they behave as expected. 
+These are not part of the ANSI Forth standard, but are common in many Forth systems. 
+If some of these words are not available or behave differently, further adjustments may be necessary.
+/////////////////////////////////////
+                    
+                    
+                    
+                    
